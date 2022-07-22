5135a160f8b14c66816b10f1ff67957e3bd661e6
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-07-22 10:19:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# com-deloitte-spring-boot-demo
+ Spring boot demo project for Deloitte trainees July 2022
---
+++ src/main/java/com/deloitte/spring/boot/demo/ComDeloitteSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.deloitte.spring.boot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComDeloitteSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComDeloitteSpringBootDemoApplication.class, args);
+	}
+
+}
