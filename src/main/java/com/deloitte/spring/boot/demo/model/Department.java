d32468f8f425b81fcd238dc7777d9e20f1fe842c
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-07-26 17:05:07 +0530

    spring boot

---
+++ src/main/java/com/deloitte/spring/boot/demo/model/Department.java
@@ -0,0 +1,13 @@
+package com.deloitte.spring.boot.demo.model;
+
+public class Department {
+
+	// Please create controller, service, repository for this entity
+
+	private int departmentId;
+	private String departmentName;
+	private String city;
+
+	// constructctors, getters, setters, toString etc
+
+}
